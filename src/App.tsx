8f7d39bb0b9ffc3ecec0eb11cd43f8c87e8412d2
Nicolas Rosal <nicolasrosal98@gmail.com>
2022-01-24 20:52:05 +0000

    Add 3D Calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,52 @@
-import { greet } from "./utils/greet";
+import { Suspense, useRef } from "react";
+import './css/App.css';
+import { Canvas,  useFrame } from '@react-three/fiber';
+import { OrbitControls } from "@react-three/drei";
+import Model from './utils/GLTFModel'
+
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+
+  const Calculator = () => {
+    const mesh = useRef<THREE.Mesh>();
+
+    useFrame(() => {
+      if (mesh.current !== undefined) {
+        mesh.current.rotation.x += 0.01
+      }
+    })
+
+    return (
+      <mesh ref={mesh}>
+        <Model />
+      </mesh>
+    );
+  };
+
+  const Lights = () => {
+    return (
+      <>
+        <ambientLight intensity={0.5} />
+        <pointLight intensity={1.0} position={[5, 3, 5]} />
+      </>
+    );
+  };
+
+  return (
+    <div className="App">
+      <Canvas>
+        <Suspense fallback={null}>
+          <OrbitControls />
+          <Calculator />
+          <Lights />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
 }
 
 export default App;
+
+// onCreated={({ gl }) => {
+    //   gl.setClearColor("#FFFFFF");
+    // }}
